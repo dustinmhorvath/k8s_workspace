29d66c5cdf4ee8dabecee51b95d3af96d766834f
Dustin Horvath <dustinmhorvath@ku.edu>
2023-01-01 21:29:21 -0600

    Adding permission for jellyfin to read books path.

--- cephx_auth.sh
+++ cephx_auth.sh
@@ -1,6 +1,6 @@
 ceph fs authorize cephfs client.transmission / r /docker/transmission rw /downloads rw
 ceph fs authorize cephfs client.jackett / r /docker/jackett rw
-ceph fs authorize cephfs client.jellyfin / r /docker/jellyfin rw /tv rw /movies rw /music rw
+ceph fs authorize cephfs client.jellyfin / r /docker/jellyfin rw /tv rw /movies rw /music rw /books rw
 ceph fs authorize cephfs client.sonarr / r /docker/sonarr rw /tv rw /downloads rw
 ceph fs authorize cephfs client.radarr / r /docker/radarr rw /movies rw /downloads rw
 ceph fs authorize cephfs client.lidarr / r /docker/lidarr rw /music rw /downloads rw /unsorted rw
@@ -10,3 +10,4 @@ ceph fs authorize cephfs client.redbot / r /docker/redbot rw
 ceph fs authorize cephfs client.samba / r /storage rw /backup rw
 
 ceph fs authorize cephfs client.docker / r /docker rw
+
