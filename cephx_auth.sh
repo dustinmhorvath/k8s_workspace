d0380673ac2d79da07ece0a762df9b62219fce19
Dustin Horvath <dustinmhorvath@gmail.com>
2025-03-10 15:29:02 -0500

    Updating a bunch of minor things. Incremental changes.

--- cephx_auth.sh
+++ cephx_auth.sh
@@ -1,16 +1,22 @@
-ceph fs authorize cephfs client.transmission / r /docker/transmission rw /downloads rw
-ceph fs authorize cephfs client.deluge / r /docker/deluge rw /downloads rw
-ceph fs authorize cephfs client.qbittorrent / r /docker/qbittorrent rw /downloads rw
-ceph fs authorize cephfs client.jackett / r /docker/jackett rw
-ceph fs authorize cephfs client.jellyfin / r /docker/jellyfin rw /tv rw /movies rw /music rw /books rw
-ceph fs authorize cephfs client.sonarr / r /docker/sonarr rw /tv rw /downloads rw
-ceph fs authorize cephfs client.radarr / r /docker/radarr rw /movies rw /downloads rw
-ceph fs authorize cephfs client.lidarr / r /docker/lidarr rw /music rw /downloads rw /unsorted rw
 ceph fs authorize cephfs client.readarr / r /docker/readarr rw /books rw /downloads rw
-ceph fs authorize cephfs client.prowlarr / r /docker/prowlarr rw
-ceph fs authorize cephfs client.redbot / r /docker/redbot rw
-ceph fs authorize cephfs client.ddclient / r /docker/ddclient rw
 ceph fs authorize cephfs client.samba / r /storage rw /backup rw
 
-ceph fs authorize cephfs client.docker / r /docker rw
 
+
+ceph fs authorize cephfs client.prowlarr / r /containervolumes/prowlarr rw /downloads rw
+ceph fs authorize cephfs client.actual / r /containervolumes/actual rw
+ceph fs authorize cephfs client.sqlite / r /containervolumes rw
+ceph fs authorize cephfs client.ddclient / r /containervolumes/ddclient rw
+ceph fs authorize cephfs client.redbot / r /containervolumes/redbot rw
+ceph fs authorize cephfs client.jellyseerr / r /containervolumes/jellyseerr rw
+ceph fs authorize cephfs client.qbittorrent / r /containervolumes/qbittorrent rw /downloads rw
+ceph fs authorize cephfs client.downloads / r /downloads rw
+ceph fs authorize cephfs client.sonarr / r /containervolumes/sonarr rw /downloads rw /tv rw
+ceph fs authorize cephfs client.radarr / r /containervolumes/radarr rw /downloads rw /movies rw
+ceph fs authorize cephfs client.sabnzbd / r /containervolumes/sabnzbd rw /downloads rw
+ceph fs authorize cephfs client.lidarr / r /containervolumes/lidarr rw /music rw /downloads rw /unsorted rw
+ceph fs authorize cephfs client.jellyfin / r /containervolumes/jellyfin rw /tv rw /movies rw /music rw /books rw
+ceph fs authorize cephfs client.satisfactory / r /containervolumes/satisfactory rw
+ceph fs authorize cephfs client.homeassistant / r /containervolumes/homeassistant rw
+ceph fs authorize cephfs client.zomboid / r /containervolumes/zomboid rw
+ceph fs authorize cephfs client.mqtt / r /containervolumes/mqtt rw
