b137f6c24197162c633370a1943c43d7e6ed02aa
Dustin Horvath <dustinmhorvath@gmail.com>
2025-12-09 13:56:42 -0600

    Changing the name of the toplevel application to 'toplevel' instead of 'applications' because of a collision.

---
+++ infrastructure/terraform/argocd_deploy_toplevel.sh
@@ -0,0 +1,4 @@
+helm --kubeconfig=kubeconfig upgrade --install argocd argo/argo-cd --create-namespace -n argocd -f argocd-install-answers.yml --wait
+KUBECONFIG=./kubeconfig argocd login --core
+KUBECONFIG=./kubeconfig kubectl config set-context --current --namespace=argocd
+KUBECONFIG=./kubeconfig argocd app create toplevel --repo https://github.com/dustinmhorvath/k8s_workspace.git --path deployments/argo --dest-server https://kubernetes.default.svc --dest-namespace argocd
